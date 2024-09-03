3cd58b38528a652e477261250a56a2c86e7e2315
Simon <work.sergey@list.ru>
2024-09-03 23:02:18 +0300

    Change maven settings and add tests

--- src/test/java/org/example/Lesson_3_1_CalculatorTest.java
+++ src/test/java/org/example/Lesson_3_1_CalculatorTest.java
@@ -30,7 +30,6 @@ class Lesson_3_1_CalculatorTest {
     @DisplayName("Проверка сложения")
     @Timeout(10)
     void sum() {
-
         Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
         int a = -10;
         int b = 10;
@@ -78,9 +77,9 @@ class Lesson_3_1_CalculatorTest {
     }
 
     @ParameterizedTest(name = "#{index} - сложение {0} и {1}, ожидаемый результат {2}")
-    @CsvSource({"1, 1, 2", "2, -2, 0", "0, 0, 0", "-1, -1, -2", "1, 1, 3"})
+    @CsvSource({"1, 1, 2", "2, -2, 0", "0, 0, 0", "-1, -1, -2", "1, 1, 2"})
     @DisplayName("Параметризованные тесты сложения")
-    void sum2(int a, int b, int expectedResult) {
+    void sum_params(int a, int b, int expectedResult) {
 
         Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
         int result = Calc.sum(a, b);
@@ -90,13 +89,79 @@ class Lesson_3_1_CalculatorTest {
     @RepeatedTest(10)
     @DisplayName("Повторяющаяся проверка сложения")
     @Timeout(10)
-    void sum3() {
+    void sum_repeat() {
 
         Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
         int a = -10;
         int b = 10;
         int result = Calc.sum(a, b);
         Assertions.assertEquals(0, result);
+    }
+
+    @ParameterizedTest(name = "#{index} - Вычитание {0} из {1}, ожидаемый результат {2}")
+    @CsvSource({"1, 1, 0", "2, -2, 4", "0, 0, 0", "-1, -1, 0", "1, 1, 0"})
+    @DisplayName("Параметризованные тесты вычитания")
+    void sub_params(int a, int b, int expectedResult) {
+
+        Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
+        int result = Calc.sub(a, b);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @RepeatedTest(10)
+    @DisplayName("Повторяющаяся проверка вычитания")
+    @Timeout(10)
+    void sub_repeat() {
+
+        Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
+        int a = -10;
+        int b = 10;
+        int result = Calc.sub(a, b);
+        Assertions.assertEquals(-20, result);
+    }
+
+    @ParameterizedTest(name = "#{index} - Умножение {0} на {1}, ожидаемый результат {2}")
+    @CsvSource({"1, 1, 1", "2, -2, -4", "0, 0, 0", "-1, -1, 1", "1, 1, 1"})
+    @DisplayName("Параметризованные тесты умножения")
+    void mult_params(int a, int b, int expectedResult) {
+
+        Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
+        int result = Calc.mult(a, b);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @RepeatedTest(10)
+    @DisplayName("Повторяющаяся проверка умножения")
+    @Timeout(10)
+    void mult_repeat() {
+
+        Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
+        int a = -10;
+        int b = 10;
+        int result = Calc.mult(a, b);
+        Assertions.assertEquals(-100, result);
+    }
+
+    @ParameterizedTest(name = "#{index} - Деление {0} на {1}, ожидаемый результат {2}")
+    @CsvSource({"1, 1, 1", "2, -2, -1", "0, 0, 0", "-6, -2, 3", "1, 1, 1"})
+    @DisplayName("Параметризованные тесты деления")
+    void div_params(int a, int b, int expectedResult) {
+
+        Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
+        int result = Calc.mult(a, b);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @RepeatedTest(10)
+    @DisplayName("Повторяющаяся проверка деления")
+    @Timeout(10)
+    void div_repeat() {
+
+        Lesson_3_1_Calculator Calc = new Lesson_3_1_Calculator();
+        int a = -10;
+        int b = 10;
+        int result = Calc.mult(a, b);
+        Assertions.assertEquals(-100, result);
     }
 
 }
--- src/test/java/org/example/TestSuite.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-
-@Suite
-
-public class TestSuite {
-
-}
