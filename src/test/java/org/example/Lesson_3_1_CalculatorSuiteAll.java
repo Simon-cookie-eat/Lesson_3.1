ab5741a2726e662ba71ad80ceb269fe5f13ba49d
Simon <work.sergey@list.ru>
2024-09-03 23:25:42 +0300

    Add suite (all)

--- src/test/java/org/example/Lesson_3_1_CalculatorSuiteAll.java
+++ src/test/java/org/example/Lesson_3_1_CalculatorSuiteAll.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import org.junit.platform.suite.api.IncludeTags;
+
+import org.junit.platform.suite.api.ExcludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectPackages("org.example")
-@IncludeTags("RepeatTest")
 
-public class Lesson_3_1_CalculatorSuiteRepeat {
+public class Lesson_3_1_CalculatorSuiteAll {
 
 }
